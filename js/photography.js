660db612e5135d1b8cea316a57bd401c93111ea9
yangchang <ha0112@livemail.tw>
2020-09-07 15:20:00 +0800

    first commit

---
+++ js/photography.js
@@ -0,0 +1,8 @@
+// nav
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+}
